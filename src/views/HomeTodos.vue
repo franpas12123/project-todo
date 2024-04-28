8911e46e991ba802fcd84fc9bad7eb7fab12c9ce
Frances Pasulohan <franpas12123@gmail.com>
2024-04-28 15:14:07 +0800

    create a view for todos

---
+++ src/views/HomeTodos.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import BaseHeader from '@/components/BaseHeader.vue'
+import ViewTodos from '@/components/todos/ViewTodos.vue'
+import AddTodo from '@/components/todos/AddTodo.vue'
+</script>
+
+<template>
+  <main>
+    <BaseHeader />
+    <AddTodo />
+    <ViewTodos />
+  </main>
+</template>
