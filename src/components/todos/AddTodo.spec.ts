46c6c639d391c442ad635c0e5f1c49ad879b6dcc
Frances Pasulohan <franpas12123@gmail.com>
2024-04-28 15:19:12 +0800

    add unit test for AddTodo component

---
+++ src/components/todos/AddTodo.spec.ts
@@ -0,0 +1,18 @@
+import { mount } from '@vue/test-utils'
+import { createTestingPinia } from '@pinia/testing'
+import AddTodo from '@/components/todos/AddTodo.vue'
+
+test('add a todo after clicking button', async () => {
+  const wrapper = mount(AddTodo, {
+    global: {
+      plugins: [createTestingPinia()]
+    }
+  })
+  const input = wrapper.find('input')
+
+  await input.setValue('Learn Vue 3')
+  await wrapper.find('button').trigger('click')
+  wrapper.vm.$nextTick()
+
+  expect(wrapper.emitted('add-todo')).toBeTruthy()
+})
