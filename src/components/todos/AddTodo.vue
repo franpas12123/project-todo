207e5a5f64314fc80c72d0af28b3105df81878a1
Frances Pasulohan <franpas12123@gmail.com>
2024-04-28 15:19:27 +0800

    create AddTodo component

---
+++ src/components/todos/AddTodo.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useTodoStore } from '@/stores/todo'
+
+const emit = defineEmits(['add-todo'])
+
+const newTodo = ref('')
+const todoStore = useTodoStore()
+const addTodo = async () => {
+  todoStore.addTodo(newTodo.value)
+  emit('add-todo')
+  newTodo.value = ''
+}
+</script>
+
+<template>
+  <div class="flex justify-around">
+    <input v-model="newTodo" type="text" name="add-todo" id="add-todo" />
+    <button @click="addTodo">Add Todo</button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
