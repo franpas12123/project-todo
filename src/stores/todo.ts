c1a4a9d64b254ba3f1171b6c2de0c831d698d28a
Frances Pasulohan <franpas12123@gmail.com>
2024-04-28 15:14:28 +0800

    create initial todo store

---
+++ src/stores/todo.ts
@@ -0,0 +1,25 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useTodoStore = defineStore('todo', () => {
+  const todos = ref<Todo[]>([])
+  const addTodo = (title): Todo => {
+    const todo = { id: todos.value.length, title, done: false, active: true }
+    todos.value.push(todo)
+    return todo
+  }
+  const toggleTodo = (index) => {
+    todos.value[index].done = !todos.value[index].done
+  }
+  const removeTodo = (index) => {
+    todos.value.splice(index, 1)
+  }
+  const seedTodos = () => {
+    console.log('Seeding todos')
+    addTodo('Learn Vue 3')
+    addTodo('Build something awesome')
+    addTodo('Take a break')
+  }
+
+  return { todos, addTodo, toggleTodo, removeTodo, seedTodos }
+})
