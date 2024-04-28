e156434286b459d8fff1b56b71c8d7dd59d13b5b
Frances Pasulohan <franpas12123@gmail.com>
2024-04-28 15:20:32 +0800

    create ViewTodos component

---
+++ src/components/todos/ViewTodos.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { useTodoStore } from '@/stores/todo'
+import type { Todo } from '@/types/Todo'
+
+const todoStore = useTodoStore()
+
+todoStore.seedTodos()
+const todos: Todo[] = todoStore.todos
+console.log(todoStore.todos)
+</script>
+
+<template>
+  <section>
+    <div v-if="todos.length" class="my-auto">
+      <h2>My Todos</h2>
+      <p>Here are the things I want to accomplish.</p>
+      <div class="todos">
+        <div v-for="(todo, index) in todos" :key="`todo-${index}`" class="todo">
+          <input v-model="todo.done" type="checkbox" :id="`todo-${index}`" />
+          <label :for="`todo-${index}`">{{ todo.title }}</label>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <p>No todos yet. Add one above!</p>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
