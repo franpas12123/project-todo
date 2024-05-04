501168e4acc9185087ec3deb3746e4f270554542
Frances Pasulohan <franpas12123@gmail.com>
2024-05-04 14:16:47 +0800

    add a test for line-through class when todo is done

--- src/components/todos/AddTodo.spec.ts
+++ src/components/todos/AddTodo.spec.ts
@@ -1,6 +1,8 @@
 import { mount } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
 import AddTodo from '@/components/todos/AddTodo.vue'
+import HomeTodos from '@/views/HomeTodos.vue'
+import { useTodoStore } from '@/stores/todo'
 
 describe('AddTodo.vue', () => {
   test('add a todo after clicking button', async () => {
@@ -16,5 +18,26 @@ describe('AddTodo.vue', () => {
     wrapper.vm.$nextTick()
 
     expect(wrapper.emitted('add-todo')).toBeTruthy()
+  })
+
+  test('line-through style when todo is done', async () => {
+    const wrapper = mount(HomeTodos, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            initialState: {
+              todo: { todos: [{ id: 0, title: 'Learn Vue 3', done: false, active: true }] }
+            }
+          })
+        ]
+      }
+    })
+    const todo = wrapper.find('label')
+    expect(todo.classes()).not.toContain('line-through')
+
+    const store = useTodoStore()
+    store.todos[0].done = true
+    await wrapper.vm.$nextTick()
+    expect(todo.classes()).toContain('line-through')
   })
 })
