aeee20ee28fa25d2b2f35a39367df4c6b2b00942
Frances Pasulohan <franpas12123@gmail.com>
2024-05-04 14:16:12 +0800

    describe AddTodo test

--- src/components/todos/AddTodo.spec.ts
+++ src/components/todos/AddTodo.spec.ts
@@ -2,17 +2,19 @@ import { mount } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
 import AddTodo from '@/components/todos/AddTodo.vue'
 
-test('add a todo after clicking button', async () => {
-  const wrapper = mount(AddTodo, {
-    global: {
-      plugins: [createTestingPinia()]
-    }
+describe('AddTodo.vue', () => {
+  test('add a todo after clicking button', async () => {
+    const wrapper = mount(AddTodo, {
+      global: {
+        plugins: [createTestingPinia()]
+      }
+    })
+    const input = wrapper.find('input')
+
+    await input.setValue('Learn Vue 3')
+    await wrapper.find('button').trigger('click')
+    wrapper.vm.$nextTick()
+
+    expect(wrapper.emitted('add-todo')).toBeTruthy()
   })
-  const input = wrapper.find('input')
-
-  await input.setValue('Learn Vue 3')
-  await wrapper.find('button').trigger('click')
-  wrapper.vm.$nextTick()
-
-  expect(wrapper.emitted('add-todo')).toBeTruthy()
 })
